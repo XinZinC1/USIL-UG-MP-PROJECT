e72d572740fdbbfc169a832491da87780519b1ed
Angel Mejia <66014666+ins4w@users.noreply.github.com>
2021-06-04 17:45:34 -0500

    Update constraint.sql

--- scripts/database/constraint.sql
+++ scripts/database/constraint.sql
@@ -6,14 +6,12 @@ ADD CONSTRAINT pk_cliente_id PRIMARY KEY (id_cliente),
     CONSTRAINT un_cliente_dni UNIQUE (dni),
     CONSTRAINT un_cliente_celular UNIQUE (celular),
     CONSTRAINT un_cliente_email UNIQUE (email);
-
 GO
 
 -- TABLA UBICACION
 
 ALTER TABLE ubicacion
 ADD CONSTRAINT pk_ubicacion_id PRIMARY KEY (id_ubicacion);
-
 GO
 
 -- TABLA ESPECIALISTAS
@@ -28,14 +26,12 @@ GO
 ALTER TABLE asunto_cita
 ADD CONSTRAINT pk_asunto_id PRIMARY KEY (id_asunto),
     CONSTRAINT fk_asunto_especialista FOREIGN KEY (id_especialista) REFERENCES especialista(id_especialista);
-
 GO
 
 -- TABLAS MÉTODOS DE PAGO
 
 ALTER TABLE metodo_pago
 ADD CONSTRAINT pk_metodo_id PRIMARY KEY (id_metodo);
-
 GO
 
 -- TABLA CITAS
@@ -46,7 +42,6 @@ ADD CONSTRAINT pk_cita_id PRIMARY KEY (id_cita),
     CONSTRAINT fk_cita_paciente FOREIGN KEY (id_paciente) REFERENCES cliente(id_cliente),
     CONSTRAINT fk_cita_asunto FOREIGN KEY (id_asunto) REFERENCES asunto_cita(id_asunto),
     CONSTRAINT fk_cita_metodo FOREIGN KEY (id_metodo) REFERENCES metodo_pago(id_metodo);
-
 GO
 
 -- TABLA BOLETAS DE CITAS
@@ -55,7 +50,6 @@ ALTER TABLE boleta_cita
 ADD CONSTRAINT pk_boleta_id PRIMARY KEY (id_boleta),
     CONSTRAINT fk_boleta_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
     CONSTRAINT fk_boleta_cita FOREIGN KEY (id_cita) REFERENCES cita(id_cita);
-
 GO
 
 -- TABLA DIAGNOSTICOS DE CITAS
@@ -64,7 +58,6 @@ ALTER TABLE diagnostico_cita
 
 ADD CONSTRAINT pk_diagnostico_id PRIMARY KEY (id_diagnostico),
     CONSTRAINT fk_diagnostico_cita FOREIGN KEY (id_cita) ReFERENCES cita(id_cita);
-
 GO
 
 -- TABLA HISTORIALES CLINICOS
