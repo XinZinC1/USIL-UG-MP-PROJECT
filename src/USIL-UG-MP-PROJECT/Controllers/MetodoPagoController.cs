2db1c93725cba77e8a7c53760835ec7b780acf6a
Angel Mejia <insaw@outlook.es>
2021-06-18 15:48:06 -0500

    Add files via upload

---
+++ src/USIL-UG-MP-PROJECT/Controllers/MetodoPagoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using USIL_UG_MP_PROJECT.Models;
+using USIL_UG_MP_PROJECT.Models.Interface;
+
+namespace USIL_UG_MP_PROJECT.Controllers
+{
+    public class MetodoPagoController : Controller
+    {
+        // Llamamos al repositorio a través de la Interfaz
+        private readonly MetodoPagoInterface _metodoPagoRepository;
+
+        // Inyectamos las dependencias tras haber agregado el servicio al Startup
+        public MetodoPagoController(MetodoPagoInterface metodoPagoRepository)
+        {
+            _metodoPagoRepository = metodoPagoRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_metodoPagoRepository.GetAllMetodosPago());
+        }
+
+        public IActionResult Insert()
+        {
+            return View();
+        }
+
+        public IActionResult InsertMetodoPago(MetodoPago metodoPago)
+        {
+            if (ModelState.IsValid)
+            {
+                _metodoPagoRepository.Add(metodoPago);
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                return View("Insert");
+            }
+        }
+
+    }
+}
---
+++ src/USIL-UG-MP-PROJECT/Controllers/UbicacionController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using USIL_UG_MP_PROJECT.Models;
+using USIL_UG_MP_PROJECT.Models.Interface;
+
+namespace USIL_UG_MP_PROJECT.Controllers
+{
+    public class UbicacionController : Controller
+    {
+        // Llamamos al repositorio a través de la Interfaz
+        private readonly UbicacionInterface _ubicacionRepository;
+
+        // Inyectamos las dependencias tras haber agregado el servicio al Startup
+        public UbicacionController(UbicacionInterface ubicacionRepository)
+        {
+            _ubicacionRepository = ubicacionRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_ubicacionRepository.GetAllUbicaciones());
+        }
+
+        public IActionResult Insert()
+        {
+            return View();
+        }
+
+        public IActionResult InsertUbicacion(Ubicacion ubicacion)
+        {
+            if (ModelState.IsValid)
+            {
+                _ubicacionRepository.Add(ubicacion);
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                return View("Insert");
+            }
+        }
+
+        public IActionResult EditUbicacion(Ubicacion ubicacion)
+        {
+            if (ModelState.IsValid)
+            {
+                _ubicacionRepository.EditDetails(ubicacion);
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                return View("Edit");
+            }
+        }
+    }
+}
